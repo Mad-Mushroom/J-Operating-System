4a04c2ea8393061b879b7d346471807542db5ea0
Mad-Mushroom <a.no.budy1@gmail.com>
2024-02-01 14:14:01 +0100

    whispering...

--- run.sh
+++ run.sh
@@ -2,12 +2,15 @@ export PATH=$PATH:/usr/local/i386elfgcc/bin
 
 nasm "src/boot/boot.asm" -f bin -o "bin/boot.bin"
 nasm "src/boot/kernel_entry.asm" -f elf -o "bin/kernel_entry.o"
-i386-elf-gcc -ffreestanding -m32 -g -c "src/kernel.c" -o "bin/c/kernel.o"
-i386-elf-gcc -ffreestanding -m32 -g -c "src/idt.c" -o "bin/c/idt.o"
+# for f in ./src/*.c; do
+#    i386-elf-gcc -ffreestanding -m32 -g -c $f -o "bin/c/$f.o"
+# done
+find ./src/*.c -type f -exec i386-elf-gcc -ffreestanding -m32 -g -c {} -o bin/c/IDK.o \;
+# i386-elf-gcc -ffreestanding -m32 -g -c "src/kernel.c" -o "bin/c/kernel.o"
 nasm "src/boot/idt.asm" -f elf -o "bin/idt.o"
 nasm "src/boot/zeroes.asm" -f bin -o "bin/zeroes.bin"
 
-i386-elf-ld -o "bin/full_kernel.bin" -Ttext 0x1000 "bin/kernel_entry.o" "bin/c/kernel.o" "bin/c/idt.o" "bin/idt.o" --oformat binary
+i386-elf-ld -o "bin/full_kernel.bin" -Ttext 0x1000 "bin/kernel_entry.o" "bin/c/*.o" "bin/idt.o" --oformat binary
 
 cat "bin/boot.bin" "bin/full_kernel.bin" "bin/zeroes.bin"  > "bin/OS.bin"
 
--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,6 @@
 #include "kernel.h"
 
-#define D_VGA_MEMORY (char*)0xb8000
-
 extern void main(){
-    *(D_VGA_MEMORY) = 'A';
+    
     return;
 }
